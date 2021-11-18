0a782219866461b3ba1857a7d8516cc9381276c9
REVE <kgw1988@naver.com>
2021-11-18 15:05:45 +0900

    first commit

---
+++ src/main/java/kr/revelope/sample/maven/polyglot/PolyglotMavenSampleApplication.java
@@ -0,0 +1,11 @@
+package kr.revelope.sample.maven.polyglot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyglotMavenSampleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PolyglotMavenSampleApplication.class, args);
+	}
+}
---
+++ src/test/java/kr/revelope/sample/maven/polyglot/PolyglotMavenSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.revelope.sample.maven.polyglot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyglotMavenSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
